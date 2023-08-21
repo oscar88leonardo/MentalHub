d29aa6a150dc8847f4f2605a0a9cd9e549eb513c
oscar88leonardo <oscar88leonardo@gmail.com>
2023-08-21 17:32:55 -0500

    pruebas huddle01 se organize setdisplayname y otros

--- MentalHub_Pages/my-app/pages/dWebRTC.tsx
+++ MentalHub_Pages/my-app/pages/dWebRTC.tsx
@@ -151,10 +151,54 @@ const App = () => {
                 >
                   JOIN_LOBY
                 </Button>
+
+                <Button disabled={!joinRoom.isCallable} onClick={joinRoom}>
+                  JOIN_ROOM
+                </Button>
+
+                <Button
+                  disabled={!fetchVideoStream.isCallable}
+                  onClick={fetchVideoStream}
+                >
+                  FETCH_VIDEO_STREAM
+                </Button> 
+
+                <Button
+                  disabled={!fetchAudioStream.isCallable}
+                  onClick={fetchAudioStream}
+                >
+                  FETCH_AUDIO_STREAM
+                </Button>
+
               </Col>
               <Col md="6" className="align-self-center ">
-                <h4 className="subtitle">Room State</h4>
-                <h6 className="subtitle break-words">{JSON.stringify(state.value)}</h6>
+                <input
+                  type="text"
+                  placeholder="Your Room Id"
+                  value={displayNameText}
+                  onChange={(e) => setDisplayNameText(e.target.value)}
+                  className="border-2 border-gray-300 bg-white h-10 px-5 pr-16 rounded-lg text-sm focus:outline-none mr-2"
+                />
+                <Button
+                  disabled={!setDisplayName.isCallable}
+                  onClick={() => {
+                    setDisplayName(displayNameText);
+                  }}
+                >
+                  {`SET_DISPLAY_NAME ${displayNameError}`}
+                </Button>  
+                <Button
+                    disabled={!stopVideoStream.isCallable}
+                    onClick={stopVideoStream}
+                  >
+                    STOP_VIDEO_STREAM
+                  </Button>
+                  <Button
+                    disabled={!stopAudioStream.isCallable}
+                    onClick={stopAudioStream}
+                  >
+                    STOP_AUDIO_STREAM
+                  </Button>                              
               </Col>
             </Row>
 
@@ -188,148 +232,102 @@ const App = () => {
                 </div>
               </Col>
               <Col md="6" className="align-self-center">
-                <div>
-                  <h4 className="subtitle">Me Id</h4>
-                  <div className="subtitle break-words">
+                  <h4 className="text-2xl">Room State</h4>
+                  <h5 className="break-words">{JSON.stringify(state.value)}</h5>
+
+                
+                  <h4 className="text-3xl text-yellow-500 font-extrabold">Lobby</h4>
+                  <h5 className="subtitle">Pj ID: {projectId}</h5>
+                  <h5 className="subtitle">Room ID: {roomId}</h5>
+            
+                  <h4 className="text-2xl">Me Id</h4>
+                  <div className="break-words">
                     {JSON.stringify(state.context.peerId)}
                   </div>
-
-                  <h4 className="subtitle">DisplayName</h4>
-                  <div className="subtitle break-words">
+                  <h4 className="text-2xl">DisplayName</h4>
+                  <div className="break-words">
                     {JSON.stringify(state.context.displayName)}
                   </div>
+                  <h4 className="text-2xl">Recording Data</h4>
+                  <div className="break-words">{JSON.stringify(recordingData)}</div>
 
-                  <h4 className="subtitle">Recording Data</h4>
-                  <div className="subtitle break-words">
-                    {JSON.stringify(recordingData)}
-                  </div>
-
-                  <h4 className="subtitle">Error</h4>
+                  <h4 className="text-2xl">Error</h4>
                   <div className="break-words text-red-500">
                     {JSON.stringify(state.context.error)}
                   </div>
-
-                  <h4 className="subtitle">Peers</h4>
-                  <div className="subtitle break-words">
-                    {JSON.stringify(peers)}
-                  </div>
-
-                  <h4 className="subtitle">Consumers</h4>
-                  <div className="subtitle break-words">
+                  <h4 className="text-2xl">Peers</h4>
+                  <div className="break-words">{JSON.stringify(peers)}</div>
+                  <h4 className="text-2xl">Consumers</h4>
+                  <div className="break-words">
                     {JSON.stringify(state.context.consumers)}
-                  </div>
-
-                </div>
+                  </div>         
+                  <Button
+                    disabled={!state.matches("Initialized.JoinedLobby")}
+                    onClick={() => send("LEAVE_LOBBY")}
+                  >
+                  LEAVE_LOBBY
+                </Button>
               </Col>
+            </Row>
+
+            <Row className="=">
+            <div>
+              <h2 className="text-3xl text-green-600 font-extrabold">Room</h2>
+              <div className="flex gap-4 flex-wrap">
+                <Button
+                  disabled={!produceAudio.isCallable}
+                  onClick={() => produceAudio(micStream)}
+                >
+                  PRODUCE_MIC
+                </Button>
+
+                <Button
+                  disabled={!produceVideo.isCallable}
+                  onClick={() => produceVideo(camStream)}
+                >
+                  PRODUCE_CAM
+                </Button>
+
+                <Button
+                  disabled={!stopProducingAudio.isCallable}
+                  onClick={() => stopProducingAudio()}
+                >
+                  STOP_PRODUCING_MIC
+                </Button>
+
+                <Button
+                  disabled={!stopProducingVideo.isCallable}
+                  onClick={() => stopProducingVideo()}
+                >
+                  STOP_PRODUCING_CAM
+                </Button>
+
+                <Button
+                  disabled={!startRecording.isCallable}
+                  onClick={() =>
+                    startRecording(`${window.location.href}rec/${roomId}`)
+                  }
+                >
+                  {`START_RECORDING ${error}`}
+                </Button>
+                <Button disabled={!stopRecording.isCallable} onClick={stopRecording}>
+                  STOP_RECORDING
+                </Button>
+
+                <Button disabled={!leaveRoom.isCallable} onClick={leaveRoom}>
+                  LEAVE_ROOM
+                </Button>
+              </div>
+
+        {/* Uncomment to see the Xstate Inspector */}
+        {/* <Inspect /> */}
+      </div>              
             </Row>
           </Container>
         </div>
       </div>
 
-      <div>
-        <br />
-        <br />
-        <h2 className="text-3xl text-yellow-500 font-extrabold">Lobby</h2>
-        <h4 className="subtitle">Pj ID: {projectId}</h4>
-        <h4 className="subtitle">Room ID: {roomId}</h4>
-        <div className="flex gap-4 flex-wrap">
-          <Button
-            disabled={!setDisplayName.isCallable}
-            onClick={() => {
-              setDisplayName(displayNameText);
-            }}
-          >
-            {`SET_DISPLAY_NAME: ${displayNameError}`}
-          </Button>
-          <Button
-            disabled={!fetchVideoStream.isCallable}
-            onClick={fetchVideoStream}
-          >
-            FETCH_VIDEO_STREAM
-          </Button>
-
-          <Button
-            disabled={!fetchAudioStream.isCallable}
-            onClick={fetchAudioStream}
-          >
-            FETCH_AUDIO_STREAM
-          </Button>
-
-          <Button disabled={!joinRoom.isCallable} onClick={joinRoom}>
-            JOIN_ROOM
-          </Button>
-
-          <Button
-            disabled={!state.matches("Initialized.JoinedLobby")}
-            onClick={() => send("LEAVE_LOBBY")}
-          >
-            LEAVE_LOBBY
-          </Button>
-
-          <Button
-            disabled={!stopVideoStream.isCallable}
-            onClick={stopVideoStream}
-          >
-            STOP_VIDEO_STREAM
-          </Button>
-          <Button
-            disabled={!stopAudioStream.isCallable}
-            onClick={stopAudioStream}
-          >
-            STOP_AUDIO_STREAM
-          </Button>
-        </div>
-        <br />
-        <h2 className="text-3xl text-green-600 font-extrabold">Room</h2>
-        <div className="flex gap-4 flex-wrap">
-          <Button
-            disabled={!produceAudio.isCallable}
-            onClick={() => produceAudio(micStream)}
-          >
-            PRODUCE_MIC
-          </Button>
-
-          <Button
-            disabled={!produceVideo.isCallable}
-            onClick={() => produceVideo(camStream)}
-          >
-            PRODUCE_CAM
-          </Button>
-
-          <Button
-            disabled={!stopProducingAudio.isCallable}
-            onClick={() => stopProducingAudio()}
-          >
-            STOP_PRODUCING_MIC
-          </Button>
-
-          <Button
-            disabled={!stopProducingVideo.isCallable}
-            onClick={() => stopProducingVideo()}
-          >
-            STOP_PRODUCING_CAM
-          </Button>
-
-          <Button
-            disabled={!startRecording.isCallable}
-            onClick={() =>
-              startRecording(`${window.location.href}rec/${roomId}`)
-            }
-          >
-            {`START_RECORDING error: ${error}`}
-          </Button>
-          <Button disabled={!stopRecording.isCallable} onClick={stopRecording}>
-            STOP_RECORDING
-          </Button>
-
-          <Button disabled={!leaveRoom.isCallable} onClick={leaveRoom}>
-            LEAVE_ROOM
-          </Button>
-        </div>
-
-        {/* Uncomment to see the Xstate Inspector */}
-        {/* <Inspect /> */}
-      </div>
+      
 
     </div>
   );
