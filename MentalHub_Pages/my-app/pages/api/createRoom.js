b5dc3703f9dd35b46299b1bc10129b495364650f
oscar88leonardo <oscar88leonardo@gmail.com>
2023-08-24 18:01:30 -0500

    test checkeo y escritura de archivos desde api

--- MentalHub_Pages/my-app/pages/api/createRoom.js
+++ MentalHub_Pages/my-app/pages/api/createRoom.js
@@ -10,8 +10,7 @@ export default async function handler(req, res) {
   const API_KEY = process.env.API_KEY;
   const HOST_WALLET = process.env.HOST_WALLET;
   //const fileroomPath = './pages/api/hdrooms.txt'
-  const fs = require("fs");
-
+  //const fs = require("fs");
 
   //Find the absolute path of the json directory
   const fileroomPath = path.join(process.cwd(), 'pages/api/hdrooms.txt');
@@ -20,36 +19,43 @@ export default async function handler(req, res) {
   //Read the json data file data.json
   //const fileContents = await fs.readFile(jsonDirectory + '/data.json', 'utf8');
 
+  const fs = require("fs");
 
-    try {                   
-          if (!fs.existsSync(fileroomPath)){
+  (async () => {
+    try {
+      fs.promises.access(fileroomPath, fs.constants.F_OK);
+      // Do something
+      const rooms = JSON.parse(fs.readFileSync(fileroomPath,'utf-8'));
+      console.log(rooms);
+      res.status(200).json(rooms);
+
+      } catch (err) {
+      
+        try{  
           const response = await axios.post(
-              'https://api.huddle01.com/api/v1/create-room',
-              {
-              title: 'MentalHub-ComTest',
-              hostWallets: [HOST_WALLET],
-              },
-              {
-              headers: {
-                  'Content-Type': 'application/json',
-                  //'x-api-key': 'g5Tcae7LDpaNHc6-sHlbuxMnahWnkcnt',
-                  'x-api-key': API_KEY,
-              },
-              }
-          );        
+          'https://api.huddle01.com/api/v1/create-room',
+          {
+          title: 'MentalHub-ComTest',
+          hostWallets: [HOST_WALLET],
+          },
+          {
+          headers: {
+              'Content-Type': 'application/json',
+              //'x-api-key': 'g5Tcae7LDpaNHc6-sHlbuxMnahWnkcnt',
+              'x-api-key': API_KEY,
+          },
+          }
+        );        
         //console.log(response.data);
         res.status(200).json(response.data.data);        
         fs.writeFileSync(fileroomPath, JSON.stringify(response.data.data) );
-        } else {
-          const rooms = JSON.parse(fs.readFileSync(fileroomPath,'utf-8'));
-          console.log(rooms);
-          res.status(200).json(rooms);
+        } catch (error) {
+               console.log(error);
+              res.status(error.response.status).json({ message: error.message });
         }
-
-      } catch (error) {
-        console.log(error);
-        res.status(error.response.status).json({ message: error.message });
-      }    
+      }  
+    })();
+               
   }
 
    
