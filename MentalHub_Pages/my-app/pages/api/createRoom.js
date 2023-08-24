d48c78f604f153bd989c8bd93f23e0d89723b42b
oscar88leonardo <oscar88leonardo@gmail.com>
2023-08-24 15:40:45 -0500

    ajustes en api para leer archivos

--- MentalHub_Pages/my-app/pages/api/createRoom.js
+++ MentalHub_Pages/my-app/pages/api/createRoom.js
@@ -1,14 +1,25 @@
 import axios from 'axios';
 import * as fs from 'fs';
 import { stringify } from 'querystring';
+import path from 'path';
+//import { promises as fs } from 'fs';
 
 
 export default async function handler(req, res) {
      
   const API_KEY = process.env.API_KEY;
   const HOST_WALLET = process.env.HOST_WALLET;
-  const fileroomPath = './pages/api/hdrooms.txt'
+  //const fileroomPath = './pages/api/hdrooms.txt'
   const fs = require("fs");
+
+
+  //Find the absolute path of the json directory
+  const fileroomPath = path.join(process.cwd(), 'pages/api/hdrooms.txt');
+  console.log("FILEPATH:");
+  console.log(fileroomPath);
+  //Read the json data file data.json
+  //const fileContents = await fs.readFile(jsonDirectory + '/data.json', 'utf8');
+
 
     try {                   
           if (!fs.existsSync(fileroomPath)){
