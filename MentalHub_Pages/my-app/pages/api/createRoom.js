f4bc41c368da19970a639db1d358360ad363a718
oscar88leonardo <oscar88leonardo@gmail.com>
2023-08-24 18:57:36 -0500

    se modifica path

--- MentalHub_Pages/my-app/pages/api/createRoom.js
+++ MentalHub_Pages/my-app/pages/api/createRoom.js
@@ -15,7 +15,7 @@ export default async function handler(req, res) {
   //Find the absolute path of the json directory
   console.log("antes de filepath");
   console.log(process.cwd());
-  const fileroomPath = path.join(process.cwd(), 'pages/api/hdrooms.txt');
+  const fileroomPath = path.join(process.cwd(), 'hdrooms.txt');
   console.log("FILEPATH:");
   console.log(fileroomPath);
   //Read the json data file data.json
