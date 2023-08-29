59b2c4bbcb90a7de20b299fc20a80c1a3dfd620c
oscar88leonardo <oscar88leonardo@gmail.com>
2023-08-29 18:49:39 -0500

    se agrega pagina de pruebas para iframe huddle01

---
+++ MentalHub_Pages/my-app/pages/iframe.js
@@ -0,0 +1,13 @@
+import { HuddleIframe } from "@huddle01/iframe";
+import Head from "next/head";
+
+export default function iframe() {
+    return (
+        
+        <div>
+            <HuddleIframe roomUrl="https://iframe.huddle01.com/mwi-axtm-zzy" className="w-full aspect-video" />
+        </div>
+
+        
+    );
+};
